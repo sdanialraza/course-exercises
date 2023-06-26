2fcc284f46fb41dfaaca872c33efc76ab6c8c737
Danial Raza <danialrazafb@gmail.com>
2023-06-26 12:25:21 +0200

    feat: add areaCirculo exercise

---
+++ ejercicios/areaCirculo/script.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} radio El radio del círculo
+ * @returns El area del circulo
+ */
+function areaCirculo(radio) {
+	return (Math.PI * Math.pow(radio, 2)).toFixed(2)
+}
+
+const radio = prompt("Introduce el radio del círculo")
+
+alert(`El area del circulo es: ${areaCirculo(radio)}`)
