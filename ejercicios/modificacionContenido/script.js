8cb6067232d0a46be6f6453631cce6c0d3a044ef
Danial Raza <danialrazafb@gmail.com>
2023-06-27 12:07:01 +0200

    feat: add modificacionContenido exercise

---
+++ ejercicios/modificacionContenido/script.js
@@ -0,0 +1,22 @@
+const h3 = document.querySelector("h3")
+
+/**
+ * @param {number} edad
+ * @param {boolean} deBarcelona
+ * @param {boolean} tieneFamiliaNumerosa
+ * @returns {void}
+ */
+function siPuedeTenerUnaBeca(edad, deBarcelona, tieneFamiliaNumerosa) {
+	const puedeTenerBeca = "Puedes tener una beca"
+	const noPuedeTenerBeca = "No puedes tener una beca"
+
+	if (edad >= 18 && deBarcelona) h3.innerText = puedeTenerBeca
+	if (edad >= 30 && tieneFamiliaNumerosa) h3.innerText = puedeTenerBeca
+	else h3.innerText = noPuedeTenerBeca
+}
+
+const edad = parseInt(prompt("¿Cuántos años tienes?"))
+const deBarcelona = confirm("¿Eres de Barcelona?")
+const tieneFamiliaNumerosa = confirm("¿Tienes familia numerosa?")
+
+siPuedeTenerUnaBeca(edad, deBarcelona, tieneFamiliaNumerosa)
