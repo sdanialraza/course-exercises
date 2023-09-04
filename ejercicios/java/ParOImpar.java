fd54d42a08823781aee04357ed586fe10fdb24e0
Danial Raza <danialrazafb@gmail.com>
2023-09-04 13:09:28 +0200

    feat: add par o impar exercise (#7)

---
+++ ejercicios/java/ParOImpar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ParOImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un numero");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) System.out.println("El numero es par");
+        else System.out.println(("El numero es impar"));
+    }
+}
