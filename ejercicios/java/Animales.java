b1d75074ca248e8ac6c8ccef77760efa403d396e
Danial Raza <danialrazafb@gmail.com>
2023-09-07 11:36:16 +0200

    feat: add animales exercise (#17)

---
+++ ejercicios/java/Animales.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Animales {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Cuantos animales quieres entrar?");
+        int cantidad = scanner.nextInt();
+
+        String[] animales = new String[cantidad];
+
+        scanner.nextLine();
+
+        for (int i = 0; i < cantidad; i++) {
+            System.out.printf("Introduce el %d animal:\n", i + 1);
+            animales[5] = scanner.nextLine();
+        }
+
+        for (String animal: animales)
+            System.out.printf("%s\n", animal);
+    }
+}
