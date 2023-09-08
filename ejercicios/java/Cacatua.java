11ecefe940f311b8b740f784bbaaf51b8a4c8e1c
Danial Raza <danialrazafb@gmail.com>
2023-09-08 10:17:03 +0200

    feat: add cacatua exercise

---
+++ ejercicios/java/Cacatua.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Cacatua {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Que comida quieres dar al cacatua?");
+        String comida = scanner.nextLine();
+
+        while (!comida.equalsIgnoreCase("Cacahuetes")) {
+            System.out.println("Cacatua cacahuete!");
+            comida = scanner.nextLine();
+            if (comida.equalsIgnoreCase("cacahuetes"))
+                System.out.println(dormirse());
+        }
+    }
+
+    public static String dormirse() {
+        return "Zzz...";
+    }
+}
