2bd9d15095efab8e98ef676225ad8b6fda5e79f8
Danial Raza <danialrazafb@gmail.com>
2023-09-14 10:24:14 +0200

    feat: add client discoteca exercise (#23)

---
+++ ejercicios/java/ClienteDiscoteca/ClienteDiscoteca.java
@@ -0,0 +1,32 @@
+package ClienteDiscoteca;
+
+public class ClienteDiscoteca {
+    private String nombre;
+    private Integer edad;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(Integer edad) {
+        if (edad >= 18) this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    @Override
+    public String toString() {
+        return """
+               ClienteDiscoteca {
+                nombre='%s',
+                edad='%d'
+               };
+               """.formatted(nombre, edad);
+    }
+}
---
+++ ejercicios/java/ClienteDiscoteca/ClientesDiscoteca.java
@@ -0,0 +1,49 @@
+package ClienteDiscoteca;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ClientesDiscoteca {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<ClienteDiscoteca> clientesDiscoteca = new ArrayList<>();
+
+        ClienteDiscoteca clienteDiscoteca = new ClienteDiscoteca();
+
+        System.out.println("Cuantas personas quieren entrar?");
+        int cantidad = scanner.nextInt();
+
+        scanner.nextLine();
+
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Como te llamas?");
+            String nombre = scanner.nextLine();
+
+            System.out.println("Cuantos años tienes?");
+            Integer edad = scanner.nextInt();
+
+            scanner.nextLine();
+
+            clienteDiscoteca.setEdad(edad);
+
+            if (edad.equals(clienteDiscoteca.getEdad())) {
+                clienteDiscoteca.setNombre(nombre);
+
+                clientesDiscoteca.add(clienteDiscoteca);
+                System.out.printf("Benivenid@s, %s (%d) a nuestra discoteca!\n", clienteDiscoteca.getNombre(), clienteDiscoteca.getEdad());
+            } else {
+                System.out.println("No puedes entrar porque no tienes 18 años o mas");
+            }
+        }
+
+        for (ClienteDiscoteca clienteDiscoteca1: clientesDiscoteca) {
+            System.out.printf(
+                    """
+                    Nombre: %s
+                    Edad: %d
+                    """,
+                    clienteDiscoteca1.getNombre(), clienteDiscoteca1.getEdad());
+        }
+    }
+}
