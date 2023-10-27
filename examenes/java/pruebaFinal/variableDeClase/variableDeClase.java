56c0ae4121c007fd8a329a6437d5dc3cf35c79e8
Danial Raza <danialrazafb@gmail.com>
2023-10-27 21:27:52 +0200

    fix: variable de clase issue

--- examenes/java/pruebaFinal/variableDeClase/variableDeClase.java
+++ examenes/java/pruebaFinal/variableDeClase/variableDeClase.java
@@ -1,60 +1,15 @@
-package variableDeClase;
+public class variableDeClase {
+    public static void main(String[] args) {
+        Estudiante[] estudiantes = {
+                new Estudiante("Juan", "Perez", "JavaScript"),
+                new Estudiante("Maria", "Gomez", "Java"),
+                new Estudiante("Pedro", "Gonzalez", "TypeScript"),
+                new Estudiante("Ana", "Garcia", "Go"),
+                new Estudiante("Luis", "Rodriguez", "Zig")
+        };
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-
-@Entity
-public class Estudiante {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String nombre;
-    private String apellido;
-    private String curso;
-
-    public Estudiante() {}
-
-    public Estudiante(String nombre, String apellido, String curso) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.curso = curso;
-    }
-
-    public int getId() {
-        return id;
-    }
-    
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public String getCurso() {
-        return curso;
-    }
-
-    public Estudiante setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public Estudiante setNombre(String nombre) {
-        this.nombre = nombre;
-        return this;
-    }
-
-    public Estudiante setApellido(String apellido) {
-        this.apellido = apellido;
-        return this;
-    }
-
-    public Estudiante setCourse(String curso) {
-        this.curso = curso;
-        return this;
+        for (Estudiante estudiante : estudiantes) {
+            System.out.println(estudiante.toString());
+        }
     }
 }
